eefa492242795a8f71da842064f52439684aff59
junhachoi <87477736+Junhachoi-DEV@users.noreply.github.com>
2023-01-27 18:45:01 +0100

    Create Test_mobile1.cs

---
+++ Assets/Scenes/Test_mobile1.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class bullet : MonoBehaviour
+{
+    
+    private void Start()
+    {
+        Debug.Log("test 완료");
+    }
+}
